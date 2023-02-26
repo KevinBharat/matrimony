ffeeb9aeb0d4885688497381e521609990b12fc4
kevin <kevinbpatoliya23@gmail.com>
2023-02-26 16:57:52 +0530

    first commit

--- app/src/test/java/com/matrimony/ExampleUnitTest.java
+++ app/src/test/java/com/matrimony/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.matrimony;
+package my.matrimony;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
